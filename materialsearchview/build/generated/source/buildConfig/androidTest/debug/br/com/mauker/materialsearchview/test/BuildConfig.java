1ed259e7e8f77b1ae7e4d3911215e1e4fb6eb868
kumailh5 <kumuh97@gmail.com>
2019-11-28 11:32:11 +0000

    update

--- materialsearchview/build/generated/source/buildConfig/androidTest/debug/br/com/mauker/materialsearchview/test/BuildConfig.java
+++ materialsearchview/build/generated/source/buildConfig/androidTest/debug/br/com/mauker/materialsearchview/test/BuildConfig.java
@@ -1,16 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package br.com.mauker.materialsearchview;
+package br.com.mauker.materialsearchview.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "br.com.mauker.materialsearchview";
-  /**
-   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
-   */
-  @Deprecated
-  public static final String APPLICATION_ID = "br.com.mauker.materialsearchview";
+  public static final String APPLICATION_ID = "br.com.mauker.materialsearchview.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 13;
